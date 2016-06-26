2e690530798741196378974bd694c0b295c4afeb
Manoel S. Motoso <manooel.s.motoso@gmail.com>
2016-06-26 11:34:44 -0300

    Codigo refatorado e  Navbar side alterada

--- app/src/main/java/br/com/manoelmotoso/tomenota/controller/NotasActivity.java
+++ app/src/main/java/br/com/manoelmotoso/tomenota/controller/NotasActivity.java
@@ -7,17 +7,19 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import com.github.brnunes.swipeablerecyclerview.SwipeableRecyclerViewTouchListener;
 
@@ -28,7 +30,7 @@ import br.com.manoelmotoso.tomenota.adapters.CardViewAdapter;
 import br.com.manoelmotoso.tomenota.dao.NotaDAO;
 import br.com.manoelmotoso.tomenota.model.Nota;
 
-public class NotasActivity extends AppCompatActivity {
+public class NotasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView mRecyclerView;
     private CardViewAdapter mAdapter;
     private List<Nota> notas;
@@ -45,14 +47,14 @@ public class NotasActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         fabDeletaNota = (FloatingActionButton) findViewById(R.id.deletaNota);
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        addItensNaLista();
         setupDrawer();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         carregarLista();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);getSupportActionBar().setHomeButtonEnabled(true);
 
         //Implementando animação e eventos  Swipe RecycleView
         SwipeableRecyclerViewTouchListener swipeTouchListener = new SwipeableRecyclerViewTouchListener(mRecyclerView, new SwipeableRecyclerViewTouchListener.SwipeListener() {
@@ -120,37 +122,27 @@ public class NotasActivity extends AppCompatActivity {
         });
     }
 
-    private void addItensNaLista() {
-        String[] itens = {"UM","DOIS","TRES","QUATRO"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itens);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-    }
 
     //Configuarando o Drawer Layout
     private void setupDrawer() {
-         toggle = new ActionBarDrawerToggle(this, drawer ,R.string.drawer_aberto, R.string.drawer_fechado){
-             @Override
-             public void onDrawerOpened(View drawerView) {
-                 super.onDrawerOpened(drawerView);
-                 toggle.setDrawerIndicatorEnabled(true);
-                 invalidateOptionsMenu();
-             }
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer ,R.string.drawer_aberto, R.string.drawer_fechado){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                toggle.syncState();
+            }
 
-             @Override
-             public void onDrawerClosed(View drawerView) {
-                 super.onDrawerClosed(drawerView);
-                 invalidateOptionsMenu();
-             }
-         };
-
-        toggle.syncState();
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                toggle.setDrawerIndicatorEnabled(true);
+            }
+        };
         drawer.setDrawerListener(toggle);
-        drawer.openDrawer(GravityCompat.START);
+        toggle.syncState();
 
     }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (toggle.onOptionsItemSelected(item)){
@@ -158,6 +150,21 @@ public class NotasActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_manage){
+            Toast.makeText(NotasActivity.this, "NAV_MANAGE", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_send){
+            Toast.makeText(NotasActivity.this, "NAV_SEND", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_share){
+            Toast.makeText(NotasActivity.this, "NAV_SHARE", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 
     //Recarrega a RecycleView de notas.
     private void carregarLista() {
@@ -169,6 +176,16 @@ public class NotasActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
     }
 
+
+    @Override
+    public void onBackPressed() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
--- app/src/main/java/br/com/manoelmotoso/tomenota/controller/NovaNotaActivity.java
+++ app/src/main/java/br/com/manoelmotoso/tomenota/controller/NovaNotaActivity.java
@@ -3,7 +3,9 @@ package br.com.manoelmotoso.tomenota.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,8 +23,8 @@ public class NovaNotaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_nova_nota);
+
         edTitulo = (EditText) findViewById(R.id.titulo);
         edDescricao = (EditText) findViewById(R.id.descricao);
         nota = new Nota();
